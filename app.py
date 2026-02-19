c2c7dcee868566881c4fb11cdc608fb31ae46826
CrazyJorge <jorge.aguiar.resende@gmail.com>
2026-02-19 14:01:12 +0000

    I forgot to commit stuff so here is just my entire website

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 # TODO: Add your routes below this line
 
+@app.route('/greet/<name>')
+def greet():
+    name = request.args.get("name")
+    return render_template("greet.html", name=name)
+
+@app.route('/')
+def index():
+    name = request.args.get("name")
+    return render_template("index.html", name=name)
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/tasks')
+def tasks():
+    return render_template("tasks.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
